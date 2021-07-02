cdf03d94e6cc5696356dc0313b2da90c683f2571
yangyafei999 <1057301319@qq.com>
2021-07-02 14:48:36 +0800

    haha

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          我 <code>爱你</code> 祖国！
+          何玉林 <code>最</code> 漂亮！
         </p>
         <a
           className="App-link"
-          href="http://www.wenming.cn/specials/100/jj_52318/"
+          href="https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%A5%A5%E5%88%A9%E7%BB%99%E5%9B%BE%E7%89%87%E8%A1%A8%E6%83%85%E5%8C%85&hs=0&pn=2&spn=0&di=71060&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&ie=utf-8&oe=utf-8&cl=2&lm=-1&cs=2088386090%2C585579774&os=3278136791%2C1444605605&simid=4184820013%2C591054801&adpicid=0&lpn=0&ln=30&fr=ala&fm=&sme=&cg=&bdtype=0&oriquery=%E5%A5%A5%E5%88%A9%E7%BB%99%E5%9B%BE%E7%89%87%E8%A1%A8%E6%83%85%E5%8C%85&objurl=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%3A%2F%2Fpic.ibaotu.com%2F01%2F77%2F97%2F93i888piCvXa.jpg-0.jpg!ww700%26refer%3Dhttp%3A%2F%2Fpic.ibaotu.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1627800453%26t%3Dea865d42e18067c25655c016142191ad&fromurl=ippr_z2C%24qAzdH3FAzdH3Ftkw5p7_z%26e3Bv54AzdH3Ff7vwtAzdH3F8l99alad_z%26e3Bip4s%3Fho1%3D%25El%25bD%25l9%25E0%25bA%25Bn%25Eb%25A8%25Ac&gsm=3&islist=&querylist="
           target="_blank"
           rel="noopener noreferrer"
         >
